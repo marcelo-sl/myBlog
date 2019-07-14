41456dfa17f834f827b5dc75b80563650cae9c23
Marcelo Sousa <mar.sousa2061@gmail.com>
2019-07-14 10:10:35 -0300

    Adding Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header-container">
+                <h1>MyBlog</h1>
+            </div>
+        );
+    }
+}
