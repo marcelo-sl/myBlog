a91b7fc458c76cf867803b559ca4b7cd58a0cea0
Marcelo Sousa <mar.sousa2061@gmail.com>
2019-07-29 17:23:09 -0300

    Database Connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
@@ -9,4 +13,6 @@ app.get('/', (req, res) => {
   res.send('My app home');
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
